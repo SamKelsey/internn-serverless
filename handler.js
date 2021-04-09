00a9300fa38463662b569faad2d9e6f846883869
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2021-04-09 14:43:02 +0100

    implemented AWS SES contactMailer function.

--- handler.js
+++ handler.js
@@ -1,14 +1,44 @@
-export const hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Go Serverless v2.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
-    }),
-  };
+const AWS = require("aws-sdk");
+const SES = new AWS.SES();
+
+export const contactMailer = async (event, context, callback) => {
+  const formData = JSON.parse(event.body);
+
+  sendEmail(formData, (err, data) => {
+    const response = {
+      statusCode: err ? 500 : 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: err ? err.message : data,
+      }),
+    };
+
+    callback(null, response);
+  });
 };
 
-const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
-  setTimeout(() => {
-    resolve(`${rest.copy} (with a delay)`);
-  }, time * 1000)
-);
+const sendEmail = (formData, callback) => {
+  const emailParams = {
+    Source: "no-reply@internn.co.uk",
+    ReplyToAddresses: [formData.reply_to],
+    Destination: {
+      ToAddresses: ["enquiries@internn.co.uk"],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Charset: "UTF-8",
+          Data: `${formData.message}\n\nName: ${formData.name}\nEmail: ${formData.reply_to}`,
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "New Enquiry",
+      },
+    },
+  };
+
+  SES.sendEmail(emailParams, callback);
+};
